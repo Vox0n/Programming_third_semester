7a6d447e90c557c6c50a4a1c788b684f1edf7d96
Vox0n <159536858+Vox0n@users.noreply.github.com>
2025-12-25 05:51:34 +0300

    Update database.py

--- LR4/python-glossary-api/app/database.py
+++ LR4/python-glossary-api/app/database.py
@@ -2,23 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Создаем файл базы данных "glossary.db" в текущей папке
 SQLALCHEMY_DATABASE_URL = "sqlite:///./glossary.db"
 
-# Создаем "движок" для подключения к БД
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-# Создаем фабрику для сессий работы с БД
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Базовый класс для создания моделей (таблиц) БД
 Base = declarative_base()
-
-# Функция для получения сессии БД (используется в эндпоинтах FastAPI)
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
